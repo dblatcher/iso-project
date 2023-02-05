e02d4ba1a500e6b283f7b90ba75643f3d78d674c
dblatcher <dv.bltchr@gmail.com>
2023-02-05 21:05:35 +0000

    impoer duck images

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { IsometricCanvas, IsometricRectangle, PlaneView } from '@elchininet/isometric'
 import { makeBlock } from './blockWithTexture';
-import { buildCuboid, buildCuboidWithShadow } from './cuboids';
 import { makeSprite } from './flatSprite';
 import { MapGrid } from './MapGrid';
 import { IMAGES } from './textures';
@@ -55,8 +54,8 @@ const mapGrid = new MapGrid([
         { image: IMAGES.duck, x: 0, y: 3, planeView: PlaneView.FRONT },
         { image: IMAGES.duck, x: 1, y: 3, planeView: PlaneView.SIDE },
         { image: IMAGES.duck, x: 2, y: 3, planeView: PlaneView.SIDE },
-        { image: IMAGES.duck, x: 2, y: 2, planeView: PlaneView.FRONT },
-        { image: IMAGES.duck, x: 3, y: 1, planeView: PlaneView.FRONT },
+        { image: IMAGES.duckBack, x: 2, y: 2, planeView: PlaneView.FRONT },
+        { image: IMAGES.duckBack, x: 3, y: 1, planeView: PlaneView.SIDE },
         { image: IMAGES.duck, x: 4, y: 1, planeView: PlaneView.FRONT },
     ]
 );
--- src/textures.ts
+++ src/textures.ts
@@ -3,7 +3,8 @@ import { IsometricGraphicProps, PlaneView } from "@elchininet/isometric";
 export const IMAGES = {
     wall: 'assets/brick_wall.png',
     grass: 'assets/grass.jpg',
-    duck: 'assets/duck.png',
+    duck: 'assets/duck/walk/front/1.png',
+    duckBack: 'assets/duck/walk/back/1.png',
 } as const;
 
 
