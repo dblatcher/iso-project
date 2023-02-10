e48c4eddfe06bad85811d25e4963f425be2eac13
dblatcher <dv.bltchr@gmail.com>
2023-02-10 23:51:17 +0000

    rotate the map

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { IsometricCanvas } from '@elchininet/isometric'
-import { Direction, DIRECTION } from './direction';
+import { clockwise, Direction, DIRECTION } from './direction';
 import { FigureSprite } from './figures';
 import { MapGrid } from './MapGrid';
 import { IMAGES } from './images';
 
 import { duckSprite } from './DirectionalSprite'
 
-function createScene(orientation:Direction) {
+function createScene(orientation: Direction) {
     const container = document.createElement('span')
     document.body.appendChild(container)
 
@@ -29,11 +29,12 @@ function createScene(orientation:Direction) {
     ];
 
     const mapGrid = new MapGrid([
-        [{ height: 1 }, { height: 1.2 }, { height: 1.4 }, { height: 1.6 }, {height:1.8}, { height: 2 }],
+        [{ height: 1 }, { height: 1.2 }, { height: 1.4 }, { height: 1.6 }, { height: 1.8 }, { height: 2 }],
         [{ height: 2 }, { height: 1 }, { height: 1 }, { height: 1 }],
         [{ height: 1 }, { height: 1 }, { height: 1 }, { height: 3.5, textureSide: IMAGES.wall, textureTop: IMAGES.grass }],
         [{ height: 1 }, { height: 1 }, { height: 1 }, { height: 1 }],
         [{ height: 0.8 }, { height: 0.8 }],
+        [, { height: 0.6 }, { height: 0.8 }],
         [, { height: 0.6 }, { height: 0.8 }],
     ],
         [
@@ -42,13 +43,21 @@ function createScene(orientation:Direction) {
     );
 
     mapGrid.render(canvas, orientation);
-
-    (window as Record<string, any>).canvas = canvas;
-    (window as Record<string, any>).mapGrid = mapGrid;
-
+    return { canvas, mapGrid }
 }
 
-createScene(DIRECTION.north)
-createScene(DIRECTION.east)
-createScene(DIRECTION.south)
-createScene(DIRECTION.west)
+// createScene(DIRECTION.north)
+// createScene(DIRECTION.east)
+// createScene(DIRECTION.south)
+const { canvas, mapGrid } = createScene(DIRECTION.west);
+
+const button = document.createElement('button')
+button.innerText = 'rotate';
+button.addEventListener('click', () => {
+    mapGrid.render(canvas, clockwise(mapGrid.renderOrientation))
+})
+document.body.appendChild(button)
+
+;
+(window as Record<string, any>).canvas = canvas;
+(window as Record<string, any>).mapGrid = mapGrid;
